a8e6a060efeca9ade58611dcb84dbaf93103f705
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-10-25 08:50:45 -0400

    Add project files.

---
+++ Topic 11 GUI - Inputting Text and Numbers tutorial/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Topic_11_GUI___Inputting_Text_and_Numbers_tutorial
+{
+    partial class FormInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.btnCalculatePrice = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(107, 6);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(100, 20);
+            this.txtItem.TabIndex = 1;
+            this.txtItem.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(107, 32);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(107, 58);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 3;
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(12, 9);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(86, 13);
+            this.lblItem.TabIndex = 4;
+            this.lblItem.Text = "Enter item name:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(12, 35);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(70, 13);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Enter a price:";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(12, 61);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(86, 13);
+            this.lblQuantity.TabIndex = 6;
+            this.lblQuantity.Text = "Number of items:";
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Location = new System.Drawing.Point(12, 110);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(59, 13);
+            this.lblTotalPrice.TabIndex = 7;
+            this.lblTotalPrice.Text = "Final Price:";
+            // 
+            // btnCalculatePrice
+            // 
+            this.btnCalculatePrice.Location = new System.Drawing.Point(15, 84);
+            this.btnCalculatePrice.Name = "btnCalculatePrice";
+            this.btnCalculatePrice.Size = new System.Drawing.Size(192, 23);
+            this.btnCalculatePrice.TabIndex = 8;
+            this.btnCalculatePrice.Text = "Calculate Final Price";
+            this.btnCalculatePrice.UseVisualStyleBackColor = true;
+            this.btnCalculatePrice.Click += new System.EventHandler(this.btnCalculatePrice_Click);
+            // 
+            // FormInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 131);
+            this.Controls.Add(this.btnCalculatePrice);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtItem);
+            this.Name = "FormInput";
+            this.Text = "User Input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblTotalPrice;
+        private System.Windows.Forms.Button btnCalculatePrice;
+    }
+}
+
---
+++ Topic 11 GUI - Inputting Text and Numbers tutorial/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_11_GUI___Inputting_Text_and_Numbers_tutorial
+{
+    public partial class FormInput : Form
+    {
+        public FormInput()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalculatePrice_Click(object sender, EventArgs e)
+        {
+            string item;
+            int quantity;
+            double price, totalPrice;
+            item = txtItem.Text;
+            price = Convert.ToDouble(txtPrice.Text);
+            quantity = Convert.ToInt32(txtQuantity.Text);
+
+            totalPrice = price * quantity;
+
+            lblTotalPrice.Text = "Final Price for " + quantity + " " + item + "(s) is: " + totalPrice.ToString("C");
+        }
+    }
+}
